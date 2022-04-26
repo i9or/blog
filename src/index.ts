887fb271678e4dd5b31479940c6d5b4927623ac5
Igor <dont@bzzr.me>
2022-04-27 00:00:35 +0100

    Fix formatting

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,20 @@ import { logger } from "@tinyhttp/logger";
 const app = new App();
 
 app
-	.use(logger())
-	.get("/",
-				(_, res) => {
-					res.format({
-					html: () => res.send('<h1>Hello!</h1>'),
-					text: () => res.send("hello")
-				})
-			}
-  )
-	.get("/page/:page/", (req, res) => {
-		res.status(200).send(`
-											 <h1>Some cool page</h1>
-											 <h2>URL</h2>
-											 ${req.url}
-											 <h2>Params</h2>
-											 ${JSON.stringify(req.params, null, 2)}
-										`)
-	})
-	.listen(3000, () => console.log(`🚀 Listening on http://localhost:3000`));
+  .use(logger())
+  .get("/", (_, res) => {
+    res.format({
+      html: () => res.send("<h1>Hello!</h1>"),
+      text: () => res.send("hello"),
+    });
+  })
+  .get("/page/:page/", (req, res) => {
+    res.status(200).send(`
+    <h1>Some cool page</h1>
+    <h2>URL</h2>
+    ${req.url}
+    <h2>Params</h2>
+    ${JSON.stringify(req.params, null, 2)}
+  `);
+  })
+  .listen(3000, () => console.log(`🚀 Listening on http://localhost:3000`));
