f76ddb80292ae54a3fda213a19ea8e77ced1fd80
Igor <dont@bzzr.me>
2022-12-07 12:49:08 +0000

    Add about page

---
+++ src/controllers/AboutController.ts
@@ -0,0 +1,26 @@
+import type { Request, Response } from "@tinyhttp/app";
+
+import { Layout } from "../templates/Layout";
+import { BaseController } from "../framework/BaseController";
+import { AboutPage } from "../templates/AboutPage";
+
+export class AboutController extends BaseController {
+  constructor() {
+    super();
+
+    this.router.get("/", this.index);
+  }
+
+  index = (_: Request, res: Response) => {
+    res.send(
+      Layout({
+        body: AboutPage(),
+        locals: res.locals,
+      })
+    );
+  };
+
+  static get path() {
+    return "/about";
+  }
+}
---
+++ src/templates/AboutPage.ts
@@ -0,0 +1,36 @@
+import { html } from "../utilities/html";
+
+export const AboutPage = () => html`<h1>About</h1>
+  <p>
+    Welcome to my personal technical blog about programming and technology! My
+    private digital garden, as they say nowadays. Here, you'll find posts about
+    a variety of topics, mostly about outdated stuff, probably a lot of swearing
+    and complaining. Probably the best thing to do is literally
+    <em>ignore this page</em>, you have been warned.
+  </p>
+
+  <p>
+    This is actually around a third attempt to start a technical blog. At this
+    point I've been stuck with programming for far too long, and now just trying
+    to survive the daily grind by offloading thoughts from my head into
+    something public place. So, the plan is simple — write a bunch of posts
+    about things I was never able to finish, hoping it will help me to mute
+    endless stream of thoughts and self blaming 😅. Among the topics I plan to
+    write about is low level game programming using C, computer graphics using
+    legacy OpenGL, or web development without those modern JavaScript
+    atrocities. Why? Because nostalgia is a hell of drug, and this blog is just
+    a way to return to those happy times virtually.
+  </p>
+
+  <p>
+    Also, feedback is not welcome 😂👍, that's basically why I am not planning
+    to add comment section or share my contact details (although there's a link
+    to my Mastodon somewhere on this website). Don't follow me, don't share my
+    links, don't hit that like button (there's no such button anyway). As
+    already mentioned above, <em>ignore this page</em> for the good.
+  </p>
+
+  <p>
+    Thank you for visiting my blog anyway, hope you won't any strong feelings
+    one way or the other about my hot takes and badly formatted ramblings!
+  </p> `;
