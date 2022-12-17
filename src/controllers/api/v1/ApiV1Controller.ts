8ca3b964b360ffea0e95fba6270346f2da865048
Igor <dont@bzzr.me>
2022-12-17 13:33:05 +0000

    Add CORS (#14)

--- src/controllers/api/v1/ApiV1Controller.ts
+++ src/controllers/api/v1/ApiV1Controller.ts
@@ -1,15 +1,23 @@
 import bodyParser from "body-parser";
+import cors from "cors";
 
 import { BaseController } from "../../../framework/BaseController";
 import { PostsApiController } from "./PostsApiController";
 import { UsersApiController } from "./UsersApiController";
 import { restrict } from "../utilities/restrict";
+import { CLIENT_ORIGINS } from "../../../configuration";
 
 export class ApiV1Controller extends BaseController {
   constructor() {
     super();
 
     this.router
+      .use(
+        cors({
+          credentials: true,
+          origin: CLIENT_ORIGINS,
+        })
+      )
       .use(bodyParser.json())
       .use(UsersApiController.path, new UsersApiController().router)
       .use(restrict) // Everything under this line needs authentication
