c6c4535d22814515b900ebe3c10e393d33bd96f7
Igor <dont@bzzr.me>
2022-12-19 16:29:25 +0000

    Make cookie always secure

--- src/controllers/api/utilities/cookie.ts
+++ src/controllers/api/utilities/cookie.ts
@@ -3,6 +3,6 @@ import { BLOG_DOMAIN, MORE_SECURE } from "~/configuration";
 export const COMMON_COOKIE_OPTIONS = {
   domain: BLOG_DOMAIN,
   httpOnly: true,
+  secure: true,
   ...(MORE_SECURE && { sameSite: "Strict" }),
-  ...(MORE_SECURE && { secure: true }),
 };
