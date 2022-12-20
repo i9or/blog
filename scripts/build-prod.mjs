1cf94af72ed33f3208da2b3425cf25a55310b66d
Igor <dont@bzzr.me>
2022-12-20 15:46:47 +0000

    Add version display (#22)

--- scripts/build-prod.mjs
+++ scripts/build-prod.mjs
@@ -5,5 +5,10 @@ esbuild
   .build({
     ...configuration,
     minify: true,
+    define: {
+      BLOG_APPLICATION_VERSION: process.env.BLOG_APPLICATION_VERSION
+        ? `"${process.env.BLOG_APPLICATION_VERSION}"`
+        : '"latest"',
+    },
   })
   .catch(() => process.exit(1));
