e8cf93e9c02f4eac84fbbbc074e4e293c44b4d80
Igor Pelekhan <dont@bzzr.me>
2024-08-10 11:30:05 +0100

    Add basic hit counter implementation (#61)
    * Add basic hit counter implementation
    
    * Update license
    
    * Add hit counter
    
    * Bump version

--- src/templates/HitCounter.ts
+++ src/templates/HitCounter.ts
@@ -1,19 +1,12 @@
 import { html } from "~/utilities/html";
 
-const HIT_COUNTER_LENGTH = 10;
-
-export const HitCounter = (hits: number = 0) => {
-  const hitsStr = `${hits}`;
-
-  if (hitsStr.length > HIT_COUNTER_LENGTH) {
-    throw new Error("Hit counter overflow");
-  }
-
-  const paddedHitsStr = hitsStr.padStart(HIT_COUNTER_LENGTH, "0");
-
-  return html`<div class="hit-counter">
-    ${paddedHitsStr
-      .split("")
-      .map((digit) => html`<span class="hit-counter__digit">${digit}</span>`)}
-  </div>`;
+export const HitCounter = () => {
+  return html`<section class="hitCounter">
+    <img
+      src="https://counter.ignorethis.page/count-me-in-scotty"
+      alt="I'm a hit counter, kinda"
+      height="30"
+      width="190"
+    />
+  </section>`;
 };
