5c696e6ef14a7981dd319dc17ceb215b4b618fac
Igor <dont@bzzr.me>
2025-07-29 19:39:40 +0100

    Minor fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
 import { defineConfig } from "vite";
 import * as path from "path";
 
+// NOTE: this is used only for vitest, build is done via esbuild
 export default defineConfig({
   resolve: {
     alias: {
